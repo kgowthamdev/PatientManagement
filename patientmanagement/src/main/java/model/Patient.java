584ff26e5bde3a0323c70d3d540cae966afce851
gowtham <kgowtham612@gmail.com>
2026-01-04 22:20:32 +0530

    added patient entity

---
+++ patientmanagement/src/main/java/model/Patient.java
@@ -0,0 +1,75 @@
+package model;
+
+import java.lang.annotation.Inherited;
+import java.time.LocalDate;
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Email;
+
+@Entity
+public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id; 
+ 
+    @NotNull
+    private String name;
+
+    @Email
+    @NotNull
+    @Column(unique=true)
+    private String email;
+
+    @NotNull  
+    private String address;
+
+    @NotNull
+    private LocalDate dob;
+
+    // Getters and Setters
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+}
---
+++ patientmanagement/src/test/java/com/zoro/patientmanagement/PatientmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zoro.patientmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
