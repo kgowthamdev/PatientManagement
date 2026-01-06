8d571dbe6243fbb12495371973e0e1b86b9ed487
Gowtham.k <Kgowtham612@gmail.com>
2026-01-06 22:18:22 +0530

    Added first controller class

--- patientmanagement/src/main/java/com/zoro/patientmanagement/PatientmanagementApplication.java
+++ patientmanagement/src/main/java/com/zoro/patientmanagement/PatientmanagementApplication.java
@@ -2,10 +2,8 @@ package com.zoro.patientmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan("model")
 public class PatientmanagementApplication {
 
 	public static void main(String[] args) {
--- patientmanagement/src/main/java/com/zoro/patientmanagement/service/PatientService.java
+++ patientmanagement/src/main/java/com/zoro/patientmanagement/service/PatientService.java
@@ -1,16 +1,37 @@
 package com.zoro.patientmanagement.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.zoro.patientmanagement.dto.PatientResponseDTO;
+import com.zoro.patientmanagement.model.Patient;
 import com.zoro.patientmanagement.repository.PatientRepository;
+import Mapper.PatientMapper;
 
 @Service
 public class PatientService {
 
+    // instance variable
     private PatientRepository patientRepository;
 
+    // constructor injection
     public PatientService(PatientRepository patientRepository) {
         this.patientRepository = patientRepository;
+    }
+
+    // method to find getpatient
+    public List<PatientResponseDTO> getPatients() {
+        List<Patient> patients = patientRepository.findAll();
+
+        // map to dto ( carry data from one layer to anothe) entity - how data is stored
+        // dto - how data is shared and in
+        // dto represent data sends out of appliction . dto is designed for api
+        // responsedto is for communicating with client
+        List<PatientResponseDTO> patientResponseDTOs = patients.stream().map(patient -> PatientMapper.tdDto(patient))
+                .toList();
+
+        return patientResponseDTOs;
     }
 
 }
